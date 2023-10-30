198f1d956c7c6b551cea75e2e13a5cd54d940659
matelat-dawa <cesarmatelat@gmail.com>
2023-10-30 12:30:56 +0000

    Update 30-10-2023 - 3

--- Programacion/C#/Ejercicio-3/Program.cs
+++ Programacion/C#/Ejercicio-3/Program.cs
@@ -1,21 +1,27 @@
 ﻿using System;
-namespace Ejercicio_3;
-class Program
-{
-   static void Main(string[] args)
-   {
-        int num;
-        int total = 0;
 
-        for (int i = 0; i < 3; i++)
+// Para Compilar el Código: mcs -out:main.exe main.cs
+// Para Ejecutarlo: mono main.exe
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
         {
-            Console.Write("Ingresa un Número: ");
-            num = Convert.ToInt32(Console.ReadLine());
-            if (num > 25)
+            int num;
+            int total = 0;
+
+            for (int i = 0; i < 3; i++)
             {
-                total += num;
+                Console.Write("Ingresa un Número: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num > 25)
+                {
+                    total += num;
+                }
             }
+            Console.WriteLine("La Suma de Los Números Mayores que 25 es: " + total);
         }
-        Console.WriteLine("La Suma de Los Números Mayores que 25 es: " + total);
-   }
+    }
 }
