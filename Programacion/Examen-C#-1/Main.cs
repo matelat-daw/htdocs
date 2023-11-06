4db4b6eebbb1821d1cd9fb1eb7d813365ed014ae
matelat-dawa <cesarmatelat@gmail.com>
2023-11-06 10:43:44 +0000

    Update Examen OK

--- Programacion/Examen-C#-1/Main.cs
+++ Programacion/Examen-C#-1/Main.cs
@@ -7,30 +7,35 @@ namespace Edades
 
         public static void Main(string[] args)
         {
+            const int MIN = 14;
+            const int MAX = 20;
             int num;
+            int resto = 0;
             int ages = 1; // Solo muestra el número de edad ingresada.
             int counter = 0;
             
             Console.WriteLine("Ingresa las Edades de las Personas");
             Console.WriteLine("");
-
-            do{
-                Console.Write("Edad{0}: ", ages); // Muestra Edad1:, Edad2:, etc.
-                num = Convert.ToInt32(Console.ReadLine());
-                if (num >= 14 && num <= 20)
+            Console.Write("Edad{0}: ", ages); // Muestra Edad1:, Edad2:, etc.
+            num = Convert.ToInt32(Console.ReadLine());
+            while (num != 999)
+            {
+                if (num >= MIN && num <= MAX)
                 {
                     counter++;
                 }
                 ages++; // Incrementa el número de Edad a ingresar.
+                Console.Write("Edad{0}: ", ages); // Muestra Edad1:, Edad2:, etc.
+                num = Convert.ToInt32(Console.ReadLine());
             }
-            while (num != 999);
-            if (counter == 3 || counter == 13 || counter == 23 || counter == 33) // Esto puede llegar hasta el máximo admitido por el micro procesador en el número terminado en 3.
+            resto = counter % 10;
+            if (resto != 3)
             {
-                Console.WriteLine("¡Qué Casualidad!");
+                Console.WriteLine("En Total Hay: {0} Personas de Entre {1} y {2} años.", counter, MIN, MAX);
             }
             else
             {
-                Console.WriteLine("En Total Hay: {0} Personas de Entre 14 y 20 años.", counter);
+                Console.WriteLine("¡Qué Casualidad!");
             }
         }
     }
