95b48c4b674378dbb353ff64ab5d3a9096ff5307
matelat-daw <cesarmatelat@gmail.com>
2023-11-22 13:56:50 +0000

    Update 22-11-2023 - 3

---
+++ Entornos de Desarrollo/C#/Excepciones/Código de Error/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Código_de_Error
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string result;
+            int num;
+            string exit = "s";
+
+            Console.WriteLine("Programa Tabla de Multiplicar\n");
+            Console.Write("Ingresa el Número Para ver su Tabla de Multiplicar: ");
+            while (exit == "s")
+            {
+                num = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("\n");
+                for (int i = 1; i < 11; i++)
+                {
+                    if (num * i > 75)
+                    {
+                        Console.WriteLine("Esos Valores Son Muy Altos Para Ti. ¡CUIDADO!");
+                    }
+                    Console.WriteLine("{0} x {1} = {2}", i, num, num * i);
+                }
+                Console.WriteLine("Deseas Ver Otra Tabla?, Presiona s para Continuar, Cualquier Otra Tecla Para Salir: ");
+                exit = Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
